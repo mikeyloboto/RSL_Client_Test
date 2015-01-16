ffdb90f289feee467eaa7a4df21b67a0890c8f47
Dmitrii Kondratev <dimonloboto@gmail.com>
2015-01-15 18:38:43 -0600

    Init

---
+++ RSL_Client_Test/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace RSL_Client_Test
+{
+    class Client
+    {
+        private static Boolean _isConnected;
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
